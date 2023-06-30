330ae4fc41d5d4bdd6f0f2de2b61f4803761594d
limo703 <118671992+limo703@users.noreply.github.com>
2023-06-30 22:34:01 +0300

    Spring

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] a = new int[8] {1,2,3,4,5,6,7,8};
+int[] b = new int[8] { 9, 2, 3, 4, 4, 6, 7, 8 };
+int[] c = new int[16];
+for(int i = 0; i < 16; i+=2)
+{
+    c[i] = a[i / 2];
+    c[i + 1] = b[i / 2];
+}
+foreach (int x in c) Console.Write(x);
